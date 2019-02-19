a23088cb79e94a0f4f2b13bd51a10e2948db5ead
thankshelp <taraninav.44@gmail.com>
2019-02-19 17:53:07 +0700

    start the program

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A and B:");
+
+            float a = float.Parse(Console.ReadLine());
+            float a = float.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Enter the menu item: ");
+            Console.WriteLine("1.+\n2.-\n3.*\n4./\n");
+
+            int q = int.Parse(Console.ReadLine());
+
+            switch (q)
+            { 
+                case 1:
+                    sum(a, b);
+                    break;
+                case 2:
+                    min(a, b);
+                    break;
+                case 3:
+                    umn(a, b);
+                    break;
+                case 4:
+                    del(a, b);
+                    break;
+            }
+        }
+    }
+}
